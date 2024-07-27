6d0ea43524101ecb060f0f82907d63c350d04da1
Irene Mwova <irene.mwova@student.moringaschool.com>
2024-07-27 09:48:04 +0300

    complete

--- lib/testing/student_test.py
+++ lib/testing/student_test.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
-from student import Student, ChattyStudent
-
+import sys
+import os
 import io
+
+# Add the project root directory to the Python path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.insert(0, project_root)
+
+
+from lib.student import Student, ChattyStudent
+
+
 import sys
 
 class TestStudent:
